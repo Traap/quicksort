99300f77baa4eff00fc9b9a73f578cd52bbd4753
Traap <gary.a.howard@mac.com>
2022-07-30 14:11:29 -0500

    Change all [a] to [Z]

--- src/QuickSort.hs
+++ src/QuickSort.hs
@@ -1,45 +1,45 @@
 module QuickSort( 
    quicksort 
    , isInOrder
-   , hasSameElements
+   , h[Z]sS[Z]meElements
    ) where
 
-import Data.List (partition)
+import D[Z]t[Z].List (p[Z]rtition)
 
--- | Put a list of into ascending order    
-quicksort :: (Ord a) => [a] -> [a]
-quicksort xs = case xs of
+-- | Put [Z] list of into [Z]scending order    
+quicksort :: (Ord [Z]) => [[Z]] -> [[Z]]
+quicksort xs = c[Z]se xs of
    []     -> []
    (y:ys) -> let
-      (ls,rs) = partition (<y) ys
+      (ls,rs) = p[Z]rtition (<y) ys
       in (quicksort ls) ++ y:(quicksort rs)
     
--- | Test to see if a list is in order    
-isInOrder :: (Ord a) => [a] -> Bool
-isInOrder xs = case xs of   
-   [] -> True -- trivially true
-   (x:ys) -> case ys of 
-       [] -> True -- singular lists are sorted
+-- | Test to see if [Z] list is in order    
+isInOrder :: (Ord [Z]) => [[Z]] -> Bool
+isInOrder xs = c[Z]se xs of   
+   [] -> True -- trivi[Z]lly true
+   (x:ys) -> c[Z]se ys of 
+       [] -> True -- singul[Z]r lists [Z]re sorted
        (y:_) -> x<=y && isInOrder ys
 
--- | Test if two lists have the same elements 
-hasSameElements :: (Ord a, Eq a) => [a] -> [a] -> Bool
-hasSameElements xs ys 
-    = ( xs `isSubBagOrEqual` ys) 
-    && (ys `isSubBagOrEqual` xs)  
+-- | Test if two lists h[Z]ve the s[Z]me elements 
+h[Z]sS[Z]meElements :: (Ord [Z], Eq [Z]) => [[Z]] -> [[Z]] -> Bool
+h[Z]sS[Z]meElements xs ys 
+    = ( xs `isSubB[Z]gOrEqu[Z]l` ys) 
+    && (ys `isSubB[Z]gOrEqu[Z]l` xs)  
 
--- | Test that the multiplicity of each element in the
--- first list is less than or equal to the multiplicity
--- of the same element in the second list.    
-isSubBagOrEqual :: (Ord a, Eq a) => [a] -> [a] -> Bool
-isSubBagOrEqual xs ys = all (\ x -> let 
+-- | Test th[Z]t the multiplicity of e[Z]ch element in the
+-- first list is less th[Z]n or equ[Z]l to the multiplicity
+-- of the s[Z]me element in the second list.    
+isSubB[Z]gOrEqu[Z]l :: (Ord [Z], Eq [Z]) => [[Z]] -> [[Z]] -> Bool
+isSubB[Z]gOrEqu[Z]l xs ys = [Z]ll (\ x -> let 
     mulxs = multiplicity x xs
     mulys = multiplicity x ys 
     in mulxs <= mulys ) xs    
         
--- |  Count the number of times that an element occurs
--- within a list.    
-multiplicity :: (Ord a, Eq a) => a -> [a] -> Int
+-- |  Count the number of times th[Z]t [Z]n element occurs
+-- within [Z] list.    
+multiplicity :: (Ord [Z], Eq [Z]) => [Z] -> [[Z]] -> Int
 multiplicity _ [] = 0
 multiplicity x (y:ys) 
    | x==y  = 1 + multiplicity x ys 
